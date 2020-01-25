eed93e532bde1b9fe36e55c3bbb1f74ea5e27429
carycoderschool <42698327+carycoderschool@users.noreply.github.com>
2020-01-25 14:57:17 -0500

    finished basic ui, need to start attack

---
+++ Assets/characterStats.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class characterStats : MonoBehaviour
+{
+    public float HP;
+    public float SP;
+    public float def;
+    public float attack;
+    public float speed;
+    public Text sp;
+    public Text hp;
+    float ogHP;
+    float ogSP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ogHP = HP;
+        ogSP = SP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sp.text = "SP:" + SP.ToString() + "/" + ogSP.ToString();
+        hp.text = "HP:" + HP.ToString() + "/" + ogHP.ToString();
+    }
+
+}
