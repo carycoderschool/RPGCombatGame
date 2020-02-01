34b0390236e31548f784c9258eacea021e1dbd4b
nickyDaggs <49346031+nickyDaggs@users.noreply.github.com>
2020-02-01 13:28:24 -0500

    enemy attack

--- Assets/attackScript.cs
+++ Assets/attackScript.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class attackScript : MonoBehaviour
 {
+    public objectLists lists;
+    
+    GameObject playerT;
+    GameObject enemy;
+    characterStats player;
+     float damage;
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemy = lists.enemies[0];
+        player = gameObject.GetComponent<characterStats>();
     }
 
     // Update is called once per frame
@@ -15,5 +23,37 @@ public class attackScript : MonoBehaviour
     {
         
     }
-    void Attack()
+    
+     void Attack()
+    {
+        foreach (GameObject button in lists.buttons)
+        {
+            button.GetComponent<Button>().interactable = false;
+            
+        }
+        damage = player.attack - enemy.GetComponent<enemyStats>().def;
+        enemy.GetComponent<enemyStats>().HP -= damage;
+        
+        StartCoroutine(enemyTurn()); 
+    }
+    IEnumerator enemyTurn()
+    {
+        yield return new WaitForSeconds(2f);
+
+        int ran = Random.Range(0, lists.chars.Length);
+        playerT = lists.chars[ran];
+        damage = enemy.GetComponent<enemyStats>().attack - player.def;
+        player.HP -= damage;
+        yield return new WaitForSeconds(2f);
+        foreach (GameObject button in lists.buttons)
+        {
+            button.GetComponent<Button>().interactable = true;
+        }
+    }
+    public void enemyChoose()
+    {
+        
+    }
+    
+   
 }
---
+++ Assets/objectLists.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class objectLists : MonoBehaviour
+{
+    public GameObject[] chars;
+    public GameObject[] items;
+    public GameObject[] enemies;
+    public GameObject[] buttons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemies = GameObject.FindGameObjectsWithTag("enemy");
+        chars = GameObject.FindGameObjectsWithTag("Player");
+        items = GameObject.FindGameObjectsWithTag("item");
+        buttons = GameObject.FindGameObjectsWithTag("button");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
